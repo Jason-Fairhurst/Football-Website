665696d220c190078813bd3f9efc6625adfed1df
Jason Fairhurst <fairhurstjason@gmail.com>
2024-01-22 17:13:45 +0000

    Header and Footer changes

---
+++ myscripts.js
@@ -0,0 +1,29 @@
+//Changes layout for mobile dropdown
+function mobileDropdown() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+//Dropdown shows when clicked
+function dropdown() {
+    document.getElementById("leagueDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
